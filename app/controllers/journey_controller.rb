56e8a1c08a67a4ed8e3581f58b16801fb91b11da
Alex Jegtnes <saccosekk@gmail.com>
2013-10-12 23:01:39 +0100

    Very basic data display

--- app/controllers/journey_controller.rb
+++ app/controllers/journey_controller.rb
@@ -1,5 +1,17 @@
 class JourneyController < ApplicationController
+  require 'nokogiri'
+  require 'open-uri'
 
   def index
+    url = "http://ojp.nationalrail.co.uk/service/timesandfares/THS/LDR/141013/0630/dep"
+    doc = Nokogiri::HTML(open(url))
+    @journeys = doc.css('.mtx').map do |i|
+      { 'dep' => i.at_css('.dep').text,
+        'arr' => i.at_css('.arr').text,
+        'dur' => i.at_css('.dur').text,
+        'chg' => i.at_css('.chg').text,
+        'fare' => i.at_css('.fare').text,
+         }
+      end
   end
 end
