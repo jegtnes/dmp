537e1075776e41db32beb129b4102e5cc5ab1367
Alex Jegtnes <saccosekk@gmail.com>
2013-11-20 23:41:27 +0000

    Added journey scraper call method

--- app/services/journey_scraper.rb
+++ app/services/journey_scraper.rb
@@ -5,4 +5,8 @@ class JourneyScraper
     @date = date
     @time = time
   end
+
+  def call
+    `/usr/local/bin/casperjs ../dmp-scraper/casper.js #{@dep} #{@arr} #{@date} #{@time} 2>&1 | grep -v "CoreText performance note"`
+  end
 end
