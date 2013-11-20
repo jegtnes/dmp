622720562ac0589587ddfcb00b8cb4800918277f
Alex Jegtnes <saccosekk@gmail.com>
2013-11-20 22:52:30 +0000

    Removing a lot of stuff due to Casper transition

--- app/controllers/journey_controller.rb
+++ app/controllers/journey_controller.rb
@@ -1,24 +1,3 @@
 class JourneyController < ApplicationController
-  require 'nokogiri'
-  require 'open-uri'
 
-  def index
-
-    url = "http://ojp.nationalrail.co.uk/service/timesandfares/"
-    url << params[:dep] + '/'
-    url << params[:arr] + '/'
-    url << params[:date]["day"] << params[:date]["month"] << params[:date][:year] + '/'
-    url << params[:time]["hour"] << params[:time]["minute"]
-    url << '/dep'
-
-    doc = Nokogiri::HTML(open(url))
-    @journeys = doc.css('.mtx').map do |i|
-      { 'dep' => i.at_css('.dep').text,
-        'arr' => i.at_css('.arr').text,
-        'dur' => i.at_css('.dur').text,
-        'chg' => i.at_css('.chg').text,
-        'fare' => i.at_css('.opsingle').text,
-         }
-      end
-  end
 end
