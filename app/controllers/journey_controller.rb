80555883e33308bd96070b78554024ec09756860
Alex Jegtnes <saccosekk@gmail.com>
2013-11-21 23:19:50 +0000

    Replaced date/time pickers with input fields

--- app/controllers/journey_controller.rb
+++ app/controllers/journey_controller.rb
@@ -1,7 +1,7 @@
 class JourneyController < ApplicationController
   def index
-    date = params[:date][:day] + params[:date][:month] + params[:date][:year]
-    time = params[:time][:hour] + params[:time][:minute]
+    date = params[:date]
+    time = params[:time]
     journey_scraper = JourneyScraper.new(params[:dep], params[:arr], date, time)
     @journeys = JSON.parse journey_scraper.call
   end
