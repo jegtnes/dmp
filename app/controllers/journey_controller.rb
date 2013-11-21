063a7b23be304e5fce1c684779b248a7f6a69835
Alex Jegtnes <saccosekk@gmail.com>
2013-11-21 00:05:11 +0000

    Railsified journey acquiring a bit

--- app/controllers/journey_controller.rb
+++ app/controllers/journey_controller.rb
@@ -1,3 +1,6 @@
 class JourneyController < ApplicationController
-
+  def index
+    journey_scraper = JourneyScraper.new('BRI', 'BPW', '121213', '1000')
+    @journey = journey_scraper.call
+  end
 end
