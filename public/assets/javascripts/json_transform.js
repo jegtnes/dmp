294c857f6a2a751374893434cd6093ca365fdb0d
Alex Jegtnes <saccosekk@gmail.com>
2013-11-24 20:22:57 +0000

    Tweaks & improvements to converter

--- public/assets/javascripts/json_transform.js
+++ public/assets/javascripts/json_transform.js
@@ -1,25 +1,24 @@
 var fs = require('fs');
 
-var trains = require('./trains (original).json');
+var trains = require('./station_data.json');
 
-var newName = [];
+var stationData = [];
 
 for(var i = 0; i < trains.length; i++) {
 	var code = trains[i].code;
 	var tks = trains[i].name.split(' ').concat(code);
-	newName.push(
+	stationData.push(
 		{
 			tokens: tks,
 			value: trains[i].name
 		}
 	);
 }
-console.log(newName);
 
-trains = JSON.stringify(newName);
+stations = JSON.stringify(stationData);
 
 console.log(trains);
-fs.writeFile("./rail_stations.json", trains, function(err) {
+fs.writeFile("public/assets/javascripts/rail_stations.json", stations, function(err) {
     if(err) {
         console.log(err);
     } else {
