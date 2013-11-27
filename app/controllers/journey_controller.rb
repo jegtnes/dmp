96cc3a022c66fbd60b7037209447bad560c52a0f
Alex Jegtnes <saccosekk@gmail.com>
2013-11-27 11:25:36 +0000

    More tolerant date/time picking

--- app/controllers/journey_controller.rb
+++ app/controllers/journey_controller.rb
@@ -1,7 +1,7 @@
 class JourneyController < ApplicationController
   def index
-    date = params[:date]
-    time = params[:time]
+    date = params[:date].gsub!(/\//, '')
+    time = params[:time].gsub(/:/, '')
     journey_scraper = JourneyScraper.new(params[:dep], params[:arr], date, time)
     @journeys = JSON.parse journey_scraper.call
   end
