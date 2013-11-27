90fbc80dac81d9f3fd6144f908d092e19ecc5c54
Alex Jegtnes <saccosekk@gmail.com>
2013-11-27 13:22:18 +0000

    Default formatting on date/time fields in place

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
   def index
-    @currentTimePadded = Time.now.hour.to_s.rjust(2, '0') + Time.now.min.to_s.rjust(2, '0')
+    @currentTimePadded = Time.now.hour.to_s.rjust(2, '0') + ":" + Time.now.min.to_s.rjust(2, '0')
+    @currentDate = Date.today.strftime("%d/%m/%Y")
   end
 end
