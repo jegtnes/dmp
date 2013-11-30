2b6189c9cc8493e5d287230e6119903ac1ebb892
Alex Jegtnes <saccosekk@gmail.com>
2013-11-30 23:35:47 +0000

    Implemented working station names

--- app/controllers/journey_controller.rb
+++ app/controllers/journey_controller.rb
@@ -2,7 +2,11 @@ class JourneyController < ApplicationController
   def index
     date = format_date(params[:date])
     time = format_time(params[:time])
+    dep = params[:dep]
+    arr = params[:arr]
     journey_scraper = JourneyScraper.new(params[:dep], params[:arr], date, time)
     @journeys = JSON.parse journey_scraper.call
+    @departure_station = get_station_name_from_code(dep)
+    @arrival_station = get_station_name_from_code(arr)
   end
 end
