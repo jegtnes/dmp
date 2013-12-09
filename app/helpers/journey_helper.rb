17b7bf032e1b453cc162e971bc84535072bf6702
Alex Jegtnes <saccosekk@gmail.com>
2013-12-09 17:18:25 +0000

    Tweaked number of good/mediocre/bad journeys

--- app/helpers/journey_helper.rb
+++ app/helpers/journey_helper.rb
@@ -3,10 +3,10 @@ module JourneyHelper
     best_journey_index = (length * 0.33).round
     good_journey_index = best_journey_index + (length * 0.33).round
 
-      if index.between?(0, best_journey_index)
-        class_name = "journey--better"
-      elsif index.between?(best_journey_index, good_journey_index)
-        class_name = "journey--good"
+      if index.between?(0, best_journey_index-1)
+        class_name = "journey--better #{best_journey_index} #{best_journey_index-1}"
+      elsif index.between?(best_journey_index-1, good_journey_index-1)
+        class_name = "journey--good #{good_journey_index} #{good_journey_index-1}"
       else
         class_name = "journey--bad"
       end
