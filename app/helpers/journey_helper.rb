0d590679558e530fb8d1ecb56773110d45c5bacb
Alex Jegtnes <saccosekk@gmail.com>
2013-12-09 17:01:43 +0000

    Cleaned up helper/view immensely (that @markjs rocks)

--- app/helpers/journey_helper.rb
+++ app/helpers/journey_helper.rb
@@ -1,31 +1,14 @@
 module JourneyHelper
-  def rate_journeys(journeys)
-    best_journeys = (journeys.length / 3.33333333333).to_i
-    good_journeys = best_journeys + (journeys.length / 3.33333333333).to_i
-    bad_journeys = journeys.length - good_journeys - best_journeys
-    html = ""
+  def journey_rating_class(length, index)
+    best_journey_index = (length * 0.33).round
+    good_journey_index = best_journey_index + (length * 0.33).round
 
-    journeys.each_with_index do |journey, index|
-      if index.between?(0, best_journeys)
-        html << "<tr class='journey--better'>"
-
-      elsif index.between?(best_journeys, good_journeys)
-        html << "<tr class='journey--good'>"
-
+      if index.between?(0, best_journey_index)
+        class_name = "journey--better"
+      elsif index.between?(best_journey_index, good_journey_index)
+        class_name = "journey--good"
       else
-        html << "<tr class='journey--bad'>"
+        class_name = "journey--bad"
       end
-
-      html << "
-        <td class='rating'>#{journey['rtg']}</td>
-        <td class='dep'>#{journey['dep']}</td>
-        <td class='arr'>#{journey['arr']}</td>
-        <td class='dur'>#{journey['dur']}</td>
-        <td class='chg'>#{journey['chg']}</td>
-        <td class='fare'>#{journey['fare']}</td>
-      </tr>"
-    end
-
-    html.html_safe
   end
 end
