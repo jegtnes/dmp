cb5dda5c35c3df0b4e03691913a670bae4d9f753
Alex Jegtnes <saccosekk@gmail.com>
2013-12-19 17:35:02 +0000

    Formatting, comments

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   var arrival = $('#arr'),
-  departure = $('#dep'),
-  arrHidden = $('#arr-hidden'),
-  depHidden = $('#dep-hidden'),
-  date = $('#date'),
-  time = $('#time');
-  var stationData;
+  departure   = $('#dep'),
+  arrHidden   = $('#arr-hidden'),
+  depHidden   = $('#dep-hidden'),
+  date        = $('#date'),
+  time        = $('#time'),
+  stationData = false;
 
   arrival.typeahead({
     name: 'stations',
@@ -13,6 +13,8 @@ $(document).ready(function() {
       url: 'assets/javascripts/rail_stations.json',
       ttl: 0,
       filter: function(data){
+
+        // Store all the retrieved JSON data to our own object for usage
         stationData = data;
         console.log(stationData);
         return data;
@@ -34,6 +36,7 @@ $(document).ready(function() {
   });
   time.pickatime();
 
+  // If choosing a city with several rail stations, just show user the name of the city
   departure.bind('typeahead:selected', function(obj, datum, name) {
     depHidden.val(datum.code);
     if (datum.value.match(/\(all stations\)/)) {
@@ -65,14 +68,20 @@ $(document).ready(function() {
   $('#journeyForm').on('submit', function(event) {
 
     event.preventDefault();
-    $('#patience').removeClass('visuallyhidden');
-    $('#journeyForm').addClass('visuallyhidden');
+
+    /* Show search confirmation to make wait seem shorter */
 
     $('#js-dep-conf').text(departure.val());
     $('#js-arr-conf').text(arrival.val());
     $('#js-date-conf').text(date.val());
     $('#js-time-conf').text(time.val());
 
+    $('#patience').removeClass('visuallyhidden');
+    $('#journeyForm').addClass('visuallyhidden');
+
+
+    /* If there's a shortcode available for the train (set earlier)
+      Replace the actual value of the field with the stored value */
     if (depHidden.val() !== "") {
       departure.val(depHidden.val());
       depHidden.remove();
