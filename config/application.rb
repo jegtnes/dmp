aaa319842d18d355f0a63f3066f6612edaf134be
Alex Jegtnes <saccosekk@gmail.com>
2014-04-01 01:30:25 +0100

    Setting time zone to London

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(:default, Rails.env)
 module Dmp
   class Application < Rails::Application
     config.assets.enabled = true
+    config.time_zone = 'Europe/London'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
