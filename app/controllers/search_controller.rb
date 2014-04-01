5c2bba71ad9fdd6a75ef70621cf9d66fc17fee6a
Alex Jegtnes <saccosekk@gmail.com>
2014-04-01 01:53:49 +0100

    How about now?

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    @currentTime = time_next_half_hour(Time.now)
+    @currentTime = time_next_half_hour(Time.zone.now)
     @currentDate = Date.today.strftime("%d/%m/%Y")
   end
 end
