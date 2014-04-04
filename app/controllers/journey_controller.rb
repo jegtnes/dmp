c2d7f76219bf59d64f991b0e142182cddf7b9c23
Alex Jegtnes <saccosekk@gmail.com>
2014-04-04 23:53:31 +0100

    Appeasing RuboCop

--- app/controllers/journey_controller.rb
+++ app/controllers/journey_controller.rb
@@ -1,13 +1,14 @@
+# Journey controller. Largely deals with interfacing with the scrapers
 class JourneyController < ApplicationController
   def index
     date = format_date(params[:date])
     time = format_time(params[:time])
     dep = params[:dep]
     arr = params[:arr]
-    journey_scraper = JourneyScraper.new(params[:dep], params[:arr], date, time)
+    journey_scraper = JourneyScraper.new(dep, arr, date, time)
     # TODO: This is fugly
-    @journeys = (JSON.parse journey_scraper.call("/usr/local/bin/casperjs app/assets/javascripts/scraper.js"))
-    .sort {|a,b| a["rtg"] <=> b["rtg"]}
+    @journeys = (JSON.parse journey_scraper.call('/usr/local/bin/casperjs app/assets/javascripts/scraper.js'))
+    .sort { | a, b | a['rtg'] <=> b['rtg'] }
     @departure_station = get_station_name_from_code(dep)
     @arrival_station = get_station_name_from_code(arr)
   end
