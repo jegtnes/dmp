7fac09c6d527a0a3faae1c7e2b1bda6cea06eda7
Alex Jegtnes <saccosekk@gmail.com>
2014-04-05 00:58:37 +0100

    Using environment variables to call scraper:

--- app/controllers/journey_controller.rb
+++ app/controllers/journey_controller.rb
@@ -6,7 +6,7 @@ class JourneyController < ApplicationController
     dep = params[:dep]
     arr = params[:arr]
     journey_scraper = JourneyScraper.new(dep, arr, date, time)
-    @journeys = journey_scraper.call('/usr/local/bin/casperjs app/assets/javascripts/scraper.js')
+    @journeys = journey_scraper.call("#{ENV['CASPER_PATH']} #{ENV['SCRAPER_PATH']}")
     @journeys = journey_scraper.parse(@journeys)
     @departure_station = get_station_name_from_code(dep)
     @arrival_station = get_station_name_from_code(arr)
