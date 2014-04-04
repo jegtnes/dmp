dcf0b530911a3fc19f73c10e1c1de5361c451291
Alex Jegtnes <saccosekk@gmail.com>
2014-04-05 00:02:24 +0100

    Appeasing RuboCop

--- app/helpers/journey_helper.rb
+++ app/helpers/journey_helper.rb
@@ -1,14 +1,19 @@
+# A helper method to assign an appropriate rating to a certain journey based
+# on the other alternatives available.
+# Params:
+# * length: The amount of
 module JourneyHelper
   def journey_rating_class(length, index)
-    best_journey_index = (length * 0.33).round
-    good_journey_index = best_journey_index + (length * 0.33).round
+    best_journey = (length * 0.33).round
+    good_journey = best_journey + (length * 0.33).round
 
-      if index.between?(0, best_journey_index-1)
-        class_name = "journey--better #{best_journey_index} #{best_journey_index-1}"
-      elsif index.between?(best_journey_index-1, good_journey_index-1)
-        class_name = "journey--good #{good_journey_index} #{good_journey_index-1}"
-      else
-        class_name = "journey--bad"
-      end
+    if index.between?(0, best_journey - 1)
+      class_name = "journey--better #{best_journey} #{best_journey - 1}"
+    elsif index.between?(best_journey - 1, good_journey - 1)
+      class_name = "journey--good #{good_journey} #{good_journey - 1}"
+    else
+      class_name = 'journey--bad'
+    end
+    class_name
   end
 end
